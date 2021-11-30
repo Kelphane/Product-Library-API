4d7ee8058725053692ebe117352917732c8d251c
Dom B <domlamont@yahoo.com>
2021-11-30 11:09:14 -0800

    Changed the return statement of the GET method

--- index.js
+++ index.js
@@ -7,10 +7,18 @@ app.listen(3000, function() {
     console.log("Server Started. Listening on Port 3000");
 });
 
-app.get('/', () => {return console.log("Hello World!");});
+app.get("/", (req, res) => {
+    return res.send("Hello World!");
+});
 
 app.get('/api/products', (req, res) => {
     const products = repoContext.products.findAllProducts();
     return res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+    const id = req.params.id;
+    const products = repoContext.products.findProductById(id);
+    return products;
 });
 
